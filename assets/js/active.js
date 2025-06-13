5bd296a8d829ddd8e163e6e2073c1034ba5f2350
Motabbir Hossain <rajuahmed362002@gmail.com>
2025-06-13 21:17:22 +0600

    two page done

---
+++ assets/js/active.js
@@ -0,0 +1,35 @@
+(function ($) {
+    "use strict";
+
+    if ($(".img-popup").length) {
+        var groups = {};
+        $(".img-popup").each(function () {
+            var id = parseInt($(this).attr("data-group"), 10);
+
+            if (!groups[id]) {
+                groups[id] = [];
+            }
+
+            groups[id].push(this);
+        });
+
+        $.each(groups, function () {
+            $(this).magnificPopup({
+                type: "image",
+                closeOnContentClick: true,
+                closeBtnInside: false,
+                gallery: {
+                    enabled: true
+                }
+            });
+        });
+    }
+
+    if ($(".mobile-nav__toggler").length) {
+        $(".mobile-nav__toggler").on("click", function (e) {
+            e.preventDefault();
+            $(".mobile-nav__wrapper").toggleClass("expanded");
+            $("body").toggleClass("locked");
+        });
+    }
+})(jQuery);
